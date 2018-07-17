eef53da12a451d599e42e0776d7d2d7413efcdc1
ihna <ihna@voyagerx.com>
2018-07-18 07:01:33 +0900

    display selected sketch

--- app/src/main/java/com/canvearth/canvearth/client/SketchPlacerView.java
+++ app/src/main/java/com/canvearth/canvearth/client/SketchPlacerView.java
@@ -3,14 +3,19 @@ package com.canvearth.canvearth.client;
 import android.content.Context;
 import android.databinding.BindingAdapter;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class SketchPlacerView extends AppCompatImageView {
-    private Drawable m_targetDrawable;
+    private Drawable drawable = null;
+    private Rect bound = null;
+
     public SketchPlacerView(Context context) {
         super(context);
     }
@@ -28,11 +33,22 @@ public class SketchPlacerView extends AppCompatImageView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if (drawable == null) {
+            return;
+        }
+        drawable.setBounds(bound);
+        drawable.draw(canvas);
     }
 
     public void setPhoto(Photo photo) {
-        Drawable drawable = photo.getDrawable();
-        if (drawable != null)
-            setImageDrawable(drawable);
+        drawable = photo.getDrawable();
+        int width = UI.getDisplayWidth();
+        int height = UI.getDisplayHeight();
+        bound = new Rect(
+                width / 4,
+                height / 4,
+                width * 3 / 4,
+                height * 3 / 4);
+        invalidate();
     }
 }
