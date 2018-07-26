641a318c490a98060291ad0b09eaa9b2cc4ac46a
ihna <ina@dblab.postech.ac.kr>
2018-07-26 12:47:18 +0900

    hotfix-uid

--- app/src/main/java/com/canvearth/canvearth/authorization/UserInformation.java
+++ app/src/main/java/com/canvearth/canvearth/authorization/UserInformation.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.canvearth.canvearth.utils.Configs;
+import com.canvearth.canvearth.utils.DatabaseUtils;
 import com.facebook.AccessToken;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -16,14 +17,17 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GetTokenResult;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeoutException;
 
 public class UserInformation {
     private static final String TAG = "UserInformation";
     private static final UserInformation ourInstance = new UserInformation();
-    private GetTokenResult mGetTokenResult = null;
-    private CountDownLatch tokenApplyLatch = new CountDownLatch(1);
+    private String uid;
 
     public static UserInformation getInstance() {
         return ourInstance;
@@ -34,25 +38,17 @@ public class UserInformation {
 
 
 
-    public String getToken() throws TimeoutException, InterruptedException {
+    public String getToken() throws InterruptedException {
         if (Configs.TESTING) {
             return "TEST_USER_TOKEN";
         }
-        tokenApplyLatch.await();
-        return mGetTokenResult.getToken();
+        return uid;
     }
 
     public void applyToken(FirebaseUser user) {
         Log.d(TAG, "Applied Token");
         if (!Configs.TESTING)
-            user.getIdToken(false).addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    Log.d(TAG, "Applied Token Succeed");
-                    mGetTokenResult = task.getResult();
-                    tokenApplyLatch.countDown();
-                } else {
-                    throw new RuntimeException("Could not get token");
-                }
-            });
+            uid = user.getUid();
+
     }
 }
